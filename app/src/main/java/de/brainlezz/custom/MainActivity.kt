2f94de0c572352e72007d6c00f3e56aad1011120
Ramon Schubert <ramonschubert@gmx.de>
2023-02-07 09:58:40 +0100

    added some sliders for demo application

--- app/src/main/java/de/brainlezz/custom/MainActivity.kt
+++ app/src/main/java/de/brainlezz/custom/MainActivity.kt
@@ -4,15 +4,30 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
+import com.larswerkman.lobsterpicker.OnColorListener
+import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider
 
-class MainActivity : AppCompatActivity(), CircleButtonView.OnElementClickedListener, IconProvider {
+class MainActivity : AppCompatActivity(), CircleButtonView.OnElementClickedListener, CircleButtonView.IconProvider {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var circleButtonView = findViewById<CircleButtonView>(R.id.circleButtonView)
+
+        findViewById<SeekBar>(R.id.seekBarBorderWidth).onProgressChanged{ circleButtonView.borderWidth = it }
+        findViewById<SeekBar>(R.id.seekBarSpacingWidth).onProgressChanged{ circleButtonView.elementSpacing = it }
+        findViewById<SeekBar>(R.id.seekBarElementCount).onProgressChanged{ circleButtonView.circleElementCount = it }
+        findViewById<SeekBar>(R.id.seekBarOffset).onProgressChanged{ circleButtonView.angleOffset = it.toFloat() }
+        findViewById<SeekBar>(R.id.seekBarInnerPortion).onProgressChanged{ circleButtonView.innerCirclePortion = it }
+
+        findViewById<LobsterShadeSlider>(R.id.shadeInnerColor).onColorChanged { circleButtonView.innerColor = it }
+        findViewById<LobsterShadeSlider>(R.id.shadeOuterColor).onColorChanged { circleButtonView.outerColor = it }
+        findViewById<LobsterShadeSlider>(R.id.shadeBorderColor).onColorChanged { circleButtonView.borderColor = it }
+        findViewById<LobsterShadeSlider>(R.id.shadehighlightColor).onColorChanged { circleButtonView.highlightColor = it }
 
         circleButtonView.addClickListener(this)
         circleButtonView.iconProvider = this
@@ -24,19 +39,17 @@ class MainActivity : AppCompatActivity(), CircleButtonView.OnElementClickedListe
 
     override fun getIconForIndex(index: Int): Bitmap? {
         var resourceId = 0
-        var dstHeight = 100
-        var dstWith = 100
+        var dstHeight = 50
+        var dstWith = 50
+        var icons = listOf(R.drawable.arrow_right,R.drawable.arrow_bottom,R.drawable.arrow_left, R.drawable.arrow_top)
+
         when(index){
             0 -> {
                 resourceId = R.drawable.play
-                dstWith = 200
-                dstHeight = 200
+                dstWith = 100
+                dstHeight = 100
             }
-            1 -> resourceId = R.drawable.arrow_right
-            2 -> resourceId = R.drawable.arrow_bottom
-            3 -> resourceId = R.drawable.arrow_left
-            4 -> resourceId = R.drawable.arrow_top
-
+            else -> resourceId = icons[Math.floorMod(index, 4)]
         }
         if(resourceId != 0) {
             var bitmap = BitmapFactory.decodeResource(baseContext.resources, resourceId)
@@ -45,4 +58,40 @@ class MainActivity : AppCompatActivity(), CircleButtonView.OnElementClickedListe
 
         return null
     }
+
+
+
+    //region Extensions
+    private fun Int.toDp(): Float =
+        this * resources.displayMetrics.density
+
+    private fun SeekBar.onProgressChanged(onProgressChanged: (Int) -> Unit) {
+        setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                onProgressChanged(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Nothing
+            }
+        })
+    }
+
+    private fun LobsterShadeSlider.onColorChanged(onColorChanged: (Int) -> Unit) {
+        addOnColorListener(object : OnColorListener {
+            override fun onColorChanged(color: Int) {
+                onColorChanged(color)
+            }
+
+            override fun onColorSelected(color: Int) {
+                // Nothing
+            }
+        })
+    }
+    //endregion
+
 }
